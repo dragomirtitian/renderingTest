818f98eb6cca568c02544d34746192ba7fef89c4
Titian Cernicova Dragomir <titian.dragomir@softvision.ro>
2017-05-03 12:30:50 +0300

    Simplified Rendering test

--- RenderingTest/Properties/AssemblyInfo.cs
+++ RenderingTest/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("86fb38e0-da17-4cfa-a9ec-e817930fb107")]
+[assembly: Guid("0e63dfa5-6265-4ccf-ba88-3328861ec3ef")]
 
 // Version information for an assembly consists of the following four values:
 //
